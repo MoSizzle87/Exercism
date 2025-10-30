a88414674fa2a2a29b2a9bb5ac0368b606ddf8b9
MoSizzle87 <105734122+MoSizzle87@users.noreply.github.com>
2025-10-30 15:29:34 +0100

    30/10/2025 : resistor-color v1

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,26 @@
-def color_code(color):
-    pass
+# A tuple with the list of colors
+COLORS = (
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+)
+
+
+def color_code(color: str):
+    if not color:
+        return False
+    if color not in COLORS:
+        return False
+
+    return COLORS.index(color)
 
 
 def colors():
-    pass
+    return list(COLORS)
